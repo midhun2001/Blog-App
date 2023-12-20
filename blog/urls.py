f741d2a3fec3464f93e3b470edc2e08f28e1994d
Midhun <midhunnkkn@gmail.com>
2023-12-20 22:13:01 +0530

    user authentication

--- blog/urls.py
+++ blog/urls.py
@@ -6,5 +6,10 @@ urlpatterns = [
     path('view_post/<int:pk>', views.view_post, name='view_post'),
     path('add_post/', views.add_post, name='add_post'),
     path('update_post/<int:pk>', views.update_post, name='update_post'),
-    path('delete_post/<int:pk>', views.delete_post, name='delete_post')
+    path('delete_post/<int:pk>', views.delete_post, name='delete_post'),
+
+    path('signup/', views.signup, name='signup'),
+    path('signin/', views.signin, name='signin'),
+    path('signout/', views.signout, name='signout')
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,36 +1,87 @@
 from django.shortcuts import redirect, render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 from .models import Post
-from .forms import PostForm
+from .forms import PostForm, SignUpForm
 
 
 def post_list(request):
     posts = Post.objects.all().order_by('-id')
-    return render(request, 'home.html', {'posts': posts})
+    return render(request, 'blog/home.html', {'posts': posts})
 
 
 def add_post(request):
-    form = PostForm(request.POST or None)
-    if request.method == 'POST':
-        if form.is_valid():
-            form.save()
-            return redirect('home')
-    return render(request, 'add_post.html', {'form': form})
-
-
+    if request.user.is_authenticated:
+        form = PostForm(request.POST or None)
+        if request.method == 'POST':
+            if form.is_valid():
+                new_post = form.save(commit=False)
+                new_post.creator = request.user
+                new_post.save()
+                messages.success(request, "Blog Post Created Successfully!")
+                return redirect('home')
+        return render(request, 'blog/add_post.html', {'form': form})
+    return redirect('signin')
+    
+    
 def update_post(request, pk):
     current_post = Post.objects.get(pk=pk)
     form = PostForm(request.POST or None, instance=current_post)
     if form.is_valid():
         form.save()
+        messages.success(request, "Blog Post Updated Successfully!")
         return redirect('home')
-    return render(request, 'update_post.html', {'form': form})
+    return render(request, 'blog/update_post.html', {'form': form, 'post':current_post})
+
 
 def view_post(request, pk):
     current_post = Post.objects.get(pk=pk)
-    return render(request, 'view_post.html', {'post': current_post})
+    return render(request, 'blog/view_post.html', {'post': current_post})
 
 
 def delete_post(request, pk):
     post = Post.objects.get(pk=pk)
     post.delete()
+    messages.success(request, "Blog Post Deleted Successfully!")
+    return redirect('home')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, "Account Created Successfully!")
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'blog/signup.html', {'form': form})
+
+
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Logged In Successfully!")
+            return redirect('home')
+        else:
+            messages.success(request, "There Was An Error Logging In, Please Try Again...")
+            return redirect('signin')
+    
+    return render(request, 'blog/signin.html')
+
+
+def signout(request):
+    logout(request)
+    messages.success(request, "Logged Out Successfully!")
     return redirect('home')
